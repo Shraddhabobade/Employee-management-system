45920c8f429ecc190b4970cf8328014109d0c29d
Shraddha Bobade <shraddhabobade03@gmail.com>
2024-06-07 17:58:08 +0530

    Add files via upload

---
+++ ProjectDemoMapping/src/main/java/com/tka/ProjectDemoMapping/ProjectDemoMappingApplication.java
@@ -0,0 +1,14 @@
+package com.tka.ProjectDemoMapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDemoMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDemoMappingApplication.class, args);
+		System.out.println("Application started...");
+	}
+
+}
